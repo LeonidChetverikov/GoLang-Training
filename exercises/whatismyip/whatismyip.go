8fe7974a899ba05d6695c0bb05ed4427097b0ff5
leonidchetverikov <l.chetverikov@gmail.com>
2017-11-29 23:18:26 +0200

    JsonParse

--- exercises/whatismyip/whatismyip.go
+++ exercises/whatismyip/whatismyip.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"net/http"
+	"fmt"
+	"encoding/json"
+	"io/ioutil"
+)
+type myip struct{
+	ipshow string
+}
+
 func main() {
 	// TODO: read info from "http://httpbin.org/ip" and write origin IP to stdout
-}
+
+	resp, err := http.Get("http://httpbin.org/ip")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	myip2:=new(myip)
+	response,err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal([]byte(response), &myip2)
+	if err !=nil {
+		panic(err)
+					}
+
+	fmt.Println(myip2)
+
+
+
+	}
+
+
+
